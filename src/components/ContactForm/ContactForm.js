e71230feebf536fb2cfa0b333eb62e226eff6652
albatrozzz <61119509+albatrozzz@users.noreply.github.com>
2021-11-29 15:27:31 +0200

    выполнен рефакторинг компонента ContactForm

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,30 +1,34 @@
-import { Component } from "react";
+import { useState } from "react";
 import s from './ContactForm.module.css'
 
 
-export default class ContactForm extends Component{
-    state = {
-        name: '',
-        number: ''
+export default function ContactForm(){
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+
+  const inputChange = (event) => {
+      switch(event.target.name){
+        case 'name':
+          setName(event.target.value)
+          break
+        case 'number':
+          setNumber(event.target.value)
+      }
     }
 
-    inputChange = (event) => {
-        this.setState({[event.target.name]: event.target.value})
-      }
     
-      formSubmit = (event) => {
+     const formSubmit = (event) => {
         event.preventDefault()
-        const {name, number} = this.state
-        this.props.onSubmit({name, number})
-        this.setState({
-            name: '',
-            number: ''
-        })
+        // const {name, number} = this.state
+        // this.props.onSubmit({name, number})
+        // this.setState({
+        //     name: '',
+        //     number: ''
+        // })
       }
 
-    render(){
         return (
-        <form onSubmit = {this.formSubmit} className ={s.form}>
+        <form onSubmit = {formSubmit} className ={s.form}>
           <label>
             <h3>Name</h3>
             <input
@@ -33,8 +37,8 @@ export default class ContactForm extends Component{
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
-              onChange = {this.inputChange}
-              value = {this.state.name}
+              onChange = {inputChange}
+              value = {name}
               className = {s.input}
             />
             <h3>Number</h3>
@@ -44,13 +48,12 @@ export default class ContactForm extends Component{
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
-              onChange = {this.inputChange}
-              value = {this.state.number}
+              onChange = {inputChange}
+              value = {number}
               className = {s.input}
             />
           </label>
           <button type = "submit" className = {s.button}>Add contact</button>
         </form> 
         )
-    }
 }
